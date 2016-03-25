a956218f3dc1295df6957eed5037d9f8247083cd
IronsDu <irons.du@gmail.com>
2016-03-25 16:09:11 +0800

    1:none

---
+++ src/net/NetSession.cpp
@@ -0,0 +1,19 @@
+#include "NetSession.h"
+
+void WrapAddNetSession(WrapServer::PTR server, int fd, BaseNetSession::PTR connection, int pingCheckTime, int maxRecvBufferSize)
+{
+    server->addSession(fd, [connection, server, pingCheckTime](TCPSession::PTR session){
+        connection->setSession(server, session->getSocketID(), session->getIP());
+        connection->onEnter();
+
+        server->getService()->setPingCheckTime(session->getSocketID(), pingCheckTime);
+
+        session->setCloseCallback([connection](TCPSession::PTR session){
+            connection->onClose();
+        });
+
+        session->setDataCallback([connection](TCPSession::PTR session, const char* buffer, size_t len){
+            return connection->onMsg(buffer, len);
+        });
+    }, false, maxRecvBufferSize);
+}
---
+++ src/net/NetSession.h
@@ -0,0 +1,79 @@
+#ifndef _NETSESSION_H
+#define _NETSESSION_H
+
+#include <string>
+#include "WrapTCPService.h"
+
+/*应用服务器的网络层会话对象基类*/
+class BaseNetSession
+{
+public:
+    typedef std::shared_ptr<BaseNetSession>  PTR;
+
+    BaseNetSession()
+    {
+        mServer = nullptr;
+    }
+
+    virtual ~BaseNetSession()
+    {
+        mSocketID = 0;
+    }
+
+    void    setSession(WrapServer::PTR server, int64_t socketID, const std::string& ip)
+    {
+        mServer = server;
+        mSocketID = socketID;
+        mIP = ip;
+    }
+
+    WrapServer::PTR getServer()
+    {
+        return mServer;
+    }
+
+    /*处理收到的数据*/
+    virtual size_t  onMsg(const char* buffer, size_t len) = 0;
+    /*链接建立*/
+    virtual void    onEnter() = 0;
+    /*链接断开*/
+    virtual void    onClose() = 0;
+
+    const std::string&  getIP() const
+    {
+        return mIP;
+    }
+
+    int64_t         getSocketID() const
+    {
+        return mSocketID;
+    }
+
+    void            postClose()
+    {
+        mServer->getService()->disConnect(mSocketID);
+    }
+
+    void            sendPacket(const char* data, int len, const DataSocket::PACKED_SENDED_CALLBACK& callback = nullptr)
+    {
+        mServer->getService()->send(mSocketID, DataSocket::makePacket(data, len), callback);
+    }
+
+    void            sendPacket(const DataSocket::PACKET_PTR& packet, const DataSocket::PACKED_SENDED_CALLBACK& callback = nullptr)
+    {
+        mServer->getService()->send(mSocketID, packet, callback);
+    }
+
+    EventLoop*      getEventLoop()
+    {
+        return mServer->getService()->getEventLoopBySocketID(mSocketID);
+    }
+private:
+    std::string         mIP;
+    WrapServer::PTR     mServer;
+    int64_t             mSocketID;
+};
+
+void WrapAddNetSession(WrapServer::PTR server, int fd, BaseNetSession::PTR pClient, int pingCheckTime, int maxRecvBufferSize);
+
+#endif
