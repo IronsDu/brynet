b6bad2a2e14bc5be0147b8e674572ab1991e03a8
IronsDu <irons.du@gmail.com>
2016-03-25 16:22:40 +0800

    1:none

---
+++ src/utils/ox_file.cpp
@@ -0,0 +1,38 @@
+﻿#include "platform.h"
+
+#ifdef PLATFORM_WINDOWS
+#include <direct.h>
+#include <io.h>
+
+#define ACCESS _access
+#define MKDIR(dir_name) _mkdir(dir_name)
+
+#else
+#include <stdarg.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#define ACCESS access
+#define MKDIR(dir_name) mkdir(dir_name, 0755)
+#endif
+
+#include "ox_file.h"
+
+bool ox_dir_create(const char* dir)
+{
+    bool ret = false;
+    if(ox_file_access(dir))
+    {
+        ret = true;
+    }
+    else
+    {
+        ret = (MKDIR(dir)) == 0;
+    }
+
+    return ret;
+}
+
+bool ox_file_access(const char* filename)
+{
+    return ACCESS(filename, 0) == 0;
+}
---
+++ src/utils/ox_file.h
@@ -0,0 +1,7 @@
+﻿#ifndef _OX_FILE_H
+#define _OX_FILE_H
+
+bool    ox_dir_create(const char* dir);
+bool    ox_file_access(const char* filename);
+
+#endif
