02910b774a1fe3058188f480f686457a9406acb4
IronsDu <irons.du@gmail.com>
2017-10-15 22:33:20 +0800

    add SSLHelper

---
+++ src/brynet/net/SSLHelper.cpp
@@ -0,0 +1,75 @@
+#include <brynet/net/SSLHelper.h>
+                                              
+using namespace brynet::net;
+
+SSLHelper::PTR SSLHelper::Create()
+{
+    struct make_shared_enabler : public SSLHelper {};
+    return std::make_shared<make_shared_enabler>();
+}
+
+SSLHelper::SSLHelper() noexcept
+{
+#ifdef USE_OPENSSL
+    mOpenSSLCTX = nullptr;
+#endif
+}
+
+SSLHelper::~SSLHelper() noexcept
+{
+#ifdef USE_OPENSSL
+    destroySSL();
+#endif
+}
+
+#ifdef USE_OPENSSL
+SSL_CTX* ListenThread::getOpenSSLCTX()
+{
+    return mOpenSSLCTX;
+}
+
+bool ListenThread::initSSL(const std::string& certificate, const std::string& privatekey)
+{
+    if (mOpenSSLCTX != nullptr)
+    {
+        return false;
+    }
+    if (certificate.empty() || privatekey.empty())
+    {
+        return false;
+    }
+
+    mOpenSSLCTX = SSL_CTX_new(SSLv23_server_method());
+    if (SSL_CTX_use_certificate_file(mOpenSSLCTX, certificate.c_str(), SSL_FILETYPE_PEM) <= 0)
+    {
+        SSL_CTX_free(mOpenSSLCTX);
+        mOpenSSLCTX = nullptr;
+        return false;
+    }
+
+    if (SSL_CTX_use_PrivateKey_file(mOpenSSLCTX, privatekey.c_str(), SSL_FILETYPE_PEM) <= 0)
+    {
+        SSL_CTX_free(mOpenSSLCTX);
+        mOpenSSLCTX = nullptr;
+        return false;
+    }
+
+    if (!SSL_CTX_check_private_key(mOpenSSLCTX))
+    {
+        SSL_CTX_free(mOpenSSLCTX);
+        mOpenSSLCTX = nullptr;
+        return false;
+    }
+
+    return true;
+}
+
+void ListenThread::destroySSL()
+{
+    if(mOpenSSLCTX != nullptr)
+    {
+        SSL_CTX_free(mOpenSSLCTX);
+        mOpenSSLCTX = nullptr;
+    }
+}
+#endif
---
+++ src/brynet/net/SSLHelper.h
@@ -0,0 +1,51 @@
+#ifndef BRYNET_NET_SSL_HELPER_H_
+#define BRYNET_NET_SSL_HELPER_H_
+
+#include <string>
+#include <memory>
+
+#include <brynet/utils/NonCopyable.h>
+
+#ifdef USE_OPENSSL
+
+#ifdef  __cplusplus
+extern "C" {
+#endif
+#include <openssl/ssl.h>
+#include <openssl/err.h>
+#ifdef  __cplusplus
+}
+#endif
+
+#endif
+
+namespace brynet
+{
+    namespace net
+    {
+        class SSLHelper : public NonCopyable, public std::enable_shared_from_this<SSLHelper>
+        {
+        public:
+            typedef std::shared_ptr<SSLHelper>   PTR;
+
+#ifdef USE_OPENSSL
+            bool                                initSSL(const std::string& certificate, 
+                                                        const std::string& privatekey);
+            void                                destroySSL();
+            SSL_CTX*                            getOpenSSLCTX();
+#endif
+            static  PTR                         Create();
+
+        private:
+            SSLHelper() noexcept;
+            virtual ~SSLHelper() noexcept;
+
+        private:
+#ifdef USE_OPENSSL
+            SSL_CTX*                            mOpenSSLCTX;
+#endif
+        };
+    }
+}
+
+#endif
