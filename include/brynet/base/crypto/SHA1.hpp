5e96b664411c346de2e802ed3501221ec69f1305
irons.du <irons.du@gmail.com>
2021-02-26 23:06:14 +0800

    add clang-format check in github action

--- include/brynet/base/crypto/SHA1.hpp
+++ include/brynet/base/crypto/SHA1.hpp
@@ -116,8 +116,8 @@
 #endif
 #endif
 
-#include <memory.h>
 #include <limits.h>
+#include <memory.h>
 
 #ifdef SHA1_UTILITY_FUNCTIONS
 #include <stdio.h>
@@ -174,48 +174,47 @@
 // Define variable types
 
 #ifndef UINT_8
-#ifdef _MSC_VER // Compiling with Microsoft compiler
+#ifdef _MSC_VER// Compiling with Microsoft compiler
 #define UINT_8 unsigned __int8
-#else // !_MSC_VER
+#else// !_MSC_VER
 #define UINT_8 unsigned char
-#endif // _MSC_VER
+#endif// _MSC_VER
 #endif
 
 #ifndef UINT_32
-#ifdef _MSC_VER // Compiling with Microsoft compiler
+#ifdef _MSC_VER// Compiling with Microsoft compiler
 #define UINT_32 unsigned __int32
-#else // !_MSC_VER
+#else// !_MSC_VER
 #if (ULONG_MAX == 0xFFFFFFFFUL)
 #define UINT_32 unsigned long
 #else
 #define UINT_32 unsigned int
 #endif
-#endif // _MSC_VER
-#endif // UINT_32
+#endif// _MSC_VER
+#endif// UINT_32
 
 #ifndef INT_64
-#ifdef _MSC_VER // Compiling with Microsoft compiler
+#ifdef _MSC_VER// Compiling with Microsoft compiler
 #define INT_64 __int64
-#else // !_MSC_VER
+#else// !_MSC_VER
 #define INT_64 long long
-#endif // _MSC_VER
-#endif // INT_64
+#endif// _MSC_VER
+#endif// INT_64
 
 #ifndef UINT_64
-#ifdef _MSC_VER // Compiling with Microsoft compiler
+#ifdef _MSC_VER// Compiling with Microsoft compiler
 #define UINT_64 unsigned __int64
-#else // !_MSC_VER
+#else// !_MSC_VER
 #define UINT_64 unsigned long long
-#endif // _MSC_VER
-#endif // UINT_64
+#endif// _MSC_VER
+#endif// UINT_64
 
 ///////////////////////////////////////////////////////////////////////////
 // Declare SHA-1 workspace
 
-typedef union
-{
-	UINT_8 c[64];
-	UINT_32 l[16];
+typedef union {
+    UINT_8 c[64];
+    UINT_32 l[16];
 } SHA1_WORKSPACE_BLOCK;
 
 
@@ -224,49 +223,70 @@ typedef union
 // Rotate p_val32 by p_nBits bits to the left
 #ifndef ROL32
 #ifdef _MSC_VER
-#define ROL32(p_val32,p_nBits) _rotl(p_val32,p_nBits)
+#define ROL32(p_val32, p_nBits) _rotl(p_val32, p_nBits)
 #else
-#define ROL32(p_val32,p_nBits) (((p_val32)<<(p_nBits))|((p_val32)>>(32-(p_nBits))))
+#define ROL32(p_val32, p_nBits) (((p_val32) << (p_nBits)) | ((p_val32) >> (32 - (p_nBits))))
 #endif
 #endif
 
 #ifdef SHA1_LITTLE_ENDIAN
 #define SHABLK0(i) (m_block->l[i] = \
-	(ROL32(m_block->l[i],24) & 0xFF00FF00) | (ROL32(m_block->l[i],8) & 0x00FF00FF))
+                            (ROL32(m_block->l[i], 24) & 0xFF00FF00) | (ROL32(m_block->l[i], 8) & 0x00FF00FF))
 #else
 #define SHABLK0(i) (m_block->l[i])
 #endif
 
-#define SHABLK(i) (m_block->l[i&15] = ROL32(m_block->l[(i+13)&15] ^ \
-	m_block->l[(i+8)&15] ^ m_block->l[(i+2)&15] ^ m_block->l[i&15],1))
+#define SHABLK(i) (m_block->l[i & 15] = ROL32(m_block->l[(i + 13) & 15] ^                                                       \
+                                                      m_block->l[(i + 8) & 15] ^ m_block->l[(i + 2) & 15] ^ m_block->l[i & 15], \
+                                              1))
 
 // SHA-1 rounds
-#define S_R0(v,w,x,y,z,i) {z+=((w&(x^y))^y)+SHABLK0(i)+0x5A827999+ROL32(v,5);w=ROL32(w,30);}
-#define S_R1(v,w,x,y,z,i) {z+=((w&(x^y))^y)+SHABLK(i)+0x5A827999+ROL32(v,5);w=ROL32(w,30);}
-#define S_R2(v,w,x,y,z,i) {z+=(w^x^y)+SHABLK(i)+0x6ED9EBA1+ROL32(v,5);w=ROL32(w,30);}
-#define S_R3(v,w,x,y,z,i) {z+=(((w|x)&y)|(w&x))+SHABLK(i)+0x8F1BBCDC+ROL32(v,5);w=ROL32(w,30);}
-#define S_R4(v,w,x,y,z,i) {z+=(w^x^y)+SHABLK(i)+0xCA62C1D6+ROL32(v,5);w=ROL32(w,30);}
+#define S_R0(v, w, x, y, z, i)                                            \
+    {                                                                     \
+        z += ((w & (x ^ y)) ^ y) + SHABLK0(i) + 0x5A827999 + ROL32(v, 5); \
+        w = ROL32(w, 30);                                                 \
+    }
+#define S_R1(v, w, x, y, z, i)                                           \
+    {                                                                    \
+        z += ((w & (x ^ y)) ^ y) + SHABLK(i) + 0x5A827999 + ROL32(v, 5); \
+        w = ROL32(w, 30);                                                \
+    }
+#define S_R2(v, w, x, y, z, i)                                   \
+    {                                                            \
+        z += (w ^ x ^ y) + SHABLK(i) + 0x6ED9EBA1 + ROL32(v, 5); \
+        w = ROL32(w, 30);                                        \
+    }
+#define S_R3(v, w, x, y, z, i)                                                 \
+    {                                                                          \
+        z += (((w | x) & y) | (w & x)) + SHABLK(i) + 0x8F1BBCDC + ROL32(v, 5); \
+        w = ROL32(w, 30);                                                      \
+    }
+#define S_R4(v, w, x, y, z, i)                                   \
+    {                                                            \
+        z += (w ^ x ^ y) + SHABLK(i) + 0xCA62C1D6 + ROL32(v, 5); \
+        w = ROL32(w, 30);                                        \
+    }
 
 
 class CSHA1
 {
 public:
 #ifdef SHA1_UTILITY_FUNCTIONS
-	// Different formats for ReportHash(Stl)
-	enum REPORT_TYPE
-	{
-		REPORT_HEX = 0,
-		REPORT_DIGIT = 1,
-		REPORT_HEX_SHORT = 2
-	};
+    // Different formats for ReportHash(Stl)
+    enum REPORT_TYPE
+    {
+        REPORT_HEX = 0,
+        REPORT_DIGIT = 1,
+        REPORT_HEX_SHORT = 2
+    };
 #endif
 
-	// Constructor and destructor
+    // Constructor and destructor
     CSHA1()
     {
-        (void)m_reserved0;
-        (void)m_reserved1;
-        m_block = (SHA1_WORKSPACE_BLOCK*)m_workspace;
+        (void) m_reserved0;
+        (void) m_reserved1;
+        m_block = (SHA1_WORKSPACE_BLOCK*) m_workspace;
 
         Reset();
     }
@@ -291,13 +311,13 @@ public:
         m_count[1] = 0;
     }
 
-	// Hash in binary data and strings
+    // Hash in binary data and strings
     void Update(const UINT_8* pbData, UINT_32 uLen)
     {
         UINT_32 j = ((m_count[0] >> 3) & 0x3F);
 
         if ((m_count[0] += (uLen << 3)) < (uLen << 3))
-            ++m_count[1]; // Overflow
+            ++m_count[1];// Overflow
 
         m_count[1] += (uLen >> 29);
 
@@ -313,23 +333,30 @@ public:
 
             j = 0;
         }
-        else i = 0;
+        else
+            i = 0;
 
         if ((uLen - i) != 0)
             memcpy(&m_buffer[j], &pbData[i], uLen - i);
     }
 
 #ifdef SHA1_UTILITY_FUNCTIONS
-	// Hash in file contents
+    // Hash in file contents
     bool HashFile(const TCHAR* tszFileName)
     {
-        if (tszFileName == NULL) return false;
+        if (tszFileName == NULL)
+            return false;
 
         FILE* fpIn = _tfopen(tszFileName, _T("rb"));
-        if (fpIn == NULL) return false;
+        if (fpIn == NULL)
+            return false;
 
         UINT_8* pbData = new UINT_8[SHA1_MAX_FILE_BUFFER];
-        if (pbData == NULL) { fclose(fpIn); return false; }
+        if (pbData == NULL)
+        {
+            fclose(fpIn);
+            return false;
+        }
 
         bool bSuccess = true;
         while (true)
@@ -341,7 +368,8 @@ public:
 
             if (uRead < SHA1_MAX_FILE_BUFFER)
             {
-                if (feof(fpIn) == 0) bSuccess = false;
+                if (feof(fpIn) == 0)
+                    bSuccess = false;
                 break;
             }
         }
@@ -352,7 +380,7 @@ public:
     }
 #endif
 
-	// Finalize hash; call it before using ReportHash(Stl)
+    // Finalize hash; call it before using ReportHash(Stl)
     void Final()
     {
         UINT_32 i;
@@ -360,20 +388,23 @@ public:
         UINT_8 pbFinalCount[8];
         for (i = 0; i < 8; ++i)
             pbFinalCount[i] = static_cast<UINT_8>((m_count[((i >= 4) ? 0 : 1)] >>
-            ((3 - (i & 3)) * 8)) & 0xFF); // Endian independent
+                                                   ((3 - (i & 3)) * 8)) &
+                                                  0xFF);// Endian independent
 
-        Update((UINT_8*)"\200", 1);
+        Update((UINT_8*) "\200", 1);
 
         while ((m_count[0] & 504) != 448)
-            Update((UINT_8*)"\0", 1);
+            Update((UINT_8*) "\0", 1);
 
-        Update(pbFinalCount, 8); // Cause a Transform()
+        Update(pbFinalCount, 8);// Cause a Transform()
 
         for (i = 0; i < 20; ++i)
             m_digest[i] = static_cast<UINT_8>((m_state[i >> 2] >> ((3 -
-            (i & 3)) * 8)) & 0xFF);
+                                                                    (i & 3)) *
+                                                                   8)) &
+                                              0xFF);
 
-        // Wipe variables for security reasons
+            // Wipe variables for security reasons
 #ifdef SHA1_WIPE_VARIABLES
         memset(m_buffer, 0, 64);
         memset(m_state, 0, 20);
@@ -386,7 +417,8 @@ public:
 #ifdef SHA1_UTILITY_FUNCTIONS
     bool ReportHash(TCHAR* tszReport, REPORT_TYPE rtReportType = REPORT_HEX) const
     {
-        if (tszReport == NULL) return false;
+        if (tszReport == NULL)
+            return false;
 
         TCHAR tszTemp[16];
 
@@ -413,7 +445,8 @@ public:
                 _tcscat(tszReport, tszTemp);
             }
         }
-        else return false;
+        else
+            return false;
 
         return true;
     }
@@ -421,25 +454,27 @@ public:
 
 #ifdef SHA1_STL_FUNCTIONS
     bool ReportHashStl(std::basic_string<TCHAR>& strOut, REPORT_TYPE rtReportType =
-        REPORT_HEX) const
+                                                                 REPORT_HEX) const
     {
         TCHAR tszOut[84];
         const bool bResult = ReportHash(tszOut, rtReportType);
-        if (bResult) strOut = tszOut;
+        if (bResult)
+            strOut = tszOut;
         return bResult;
     }
 #endif
 
-	// Get the raw message digest (20 bytes)
+    // Get the raw message digest (20 bytes)
     bool GetHash(UINT_8* pbDest20) const
     {
-        if (pbDest20 == NULL) return false;
+        if (pbDest20 == NULL)
+            return false;
         memcpy(pbDest20, m_digest, 20);
         return true;
     }
 
 private:
-	// Private SHA-1 transformation
+    // Private SHA-1 transformation
     void Transform(UINT_32* pState, const UINT_8* pBuffer)
     {
         UINT_32 a = pState[0], b = pState[1], c = pState[2], d = pState[3], e = pState[4];
@@ -447,26 +482,86 @@ private:
         memcpy(m_block, pBuffer, 64);
 
         // 4 rounds of 20 operations each, loop unrolled
-        S_R0(a, b, c, d, e, 0); S_R0(e, a, b, c, d, 1); S_R0(d, e, a, b, c, 2); S_R0(c, d, e, a, b, 3);
-        S_R0(b, c, d, e, a, 4); S_R0(a, b, c, d, e, 5); S_R0(e, a, b, c, d, 6); S_R0(d, e, a, b, c, 7);
-        S_R0(c, d, e, a, b, 8); S_R0(b, c, d, e, a, 9); S_R0(a, b, c, d, e, 10); S_R0(e, a, b, c, d, 11);
-        S_R0(d, e, a, b, c, 12); S_R0(c, d, e, a, b, 13); S_R0(b, c, d, e, a, 14); S_R0(a, b, c, d, e, 15);
-        S_R1(e, a, b, c, d, 16); S_R1(d, e, a, b, c, 17); S_R1(c, d, e, a, b, 18); S_R1(b, c, d, e, a, 19);
-        S_R2(a, b, c, d, e, 20); S_R2(e, a, b, c, d, 21); S_R2(d, e, a, b, c, 22); S_R2(c, d, e, a, b, 23);
-        S_R2(b, c, d, e, a, 24); S_R2(a, b, c, d, e, 25); S_R2(e, a, b, c, d, 26); S_R2(d, e, a, b, c, 27);
-        S_R2(c, d, e, a, b, 28); S_R2(b, c, d, e, a, 29); S_R2(a, b, c, d, e, 30); S_R2(e, a, b, c, d, 31);
-        S_R2(d, e, a, b, c, 32); S_R2(c, d, e, a, b, 33); S_R2(b, c, d, e, a, 34); S_R2(a, b, c, d, e, 35);
-        S_R2(e, a, b, c, d, 36); S_R2(d, e, a, b, c, 37); S_R2(c, d, e, a, b, 38); S_R2(b, c, d, e, a, 39);
-        S_R3(a, b, c, d, e, 40); S_R3(e, a, b, c, d, 41); S_R3(d, e, a, b, c, 42); S_R3(c, d, e, a, b, 43);
-        S_R3(b, c, d, e, a, 44); S_R3(a, b, c, d, e, 45); S_R3(e, a, b, c, d, 46); S_R3(d, e, a, b, c, 47);
-        S_R3(c, d, e, a, b, 48); S_R3(b, c, d, e, a, 49); S_R3(a, b, c, d, e, 50); S_R3(e, a, b, c, d, 51);
-        S_R3(d, e, a, b, c, 52); S_R3(c, d, e, a, b, 53); S_R3(b, c, d, e, a, 54); S_R3(a, b, c, d, e, 55);
-        S_R3(e, a, b, c, d, 56); S_R3(d, e, a, b, c, 57); S_R3(c, d, e, a, b, 58); S_R3(b, c, d, e, a, 59);
-        S_R4(a, b, c, d, e, 60); S_R4(e, a, b, c, d, 61); S_R4(d, e, a, b, c, 62); S_R4(c, d, e, a, b, 63);
-        S_R4(b, c, d, e, a, 64); S_R4(a, b, c, d, e, 65); S_R4(e, a, b, c, d, 66); S_R4(d, e, a, b, c, 67);
-        S_R4(c, d, e, a, b, 68); S_R4(b, c, d, e, a, 69); S_R4(a, b, c, d, e, 70); S_R4(e, a, b, c, d, 71);
-        S_R4(d, e, a, b, c, 72); S_R4(c, d, e, a, b, 73); S_R4(b, c, d, e, a, 74); S_R4(a, b, c, d, e, 75);
-        S_R4(e, a, b, c, d, 76); S_R4(d, e, a, b, c, 77); S_R4(c, d, e, a, b, 78); S_R4(b, c, d, e, a, 79);
+        S_R0(a, b, c, d, e, 0);
+        S_R0(e, a, b, c, d, 1);
+        S_R0(d, e, a, b, c, 2);
+        S_R0(c, d, e, a, b, 3);
+        S_R0(b, c, d, e, a, 4);
+        S_R0(a, b, c, d, e, 5);
+        S_R0(e, a, b, c, d, 6);
+        S_R0(d, e, a, b, c, 7);
+        S_R0(c, d, e, a, b, 8);
+        S_R0(b, c, d, e, a, 9);
+        S_R0(a, b, c, d, e, 10);
+        S_R0(e, a, b, c, d, 11);
+        S_R0(d, e, a, b, c, 12);
+        S_R0(c, d, e, a, b, 13);
+        S_R0(b, c, d, e, a, 14);
+        S_R0(a, b, c, d, e, 15);
+        S_R1(e, a, b, c, d, 16);
+        S_R1(d, e, a, b, c, 17);
+        S_R1(c, d, e, a, b, 18);
+        S_R1(b, c, d, e, a, 19);
+        S_R2(a, b, c, d, e, 20);
+        S_R2(e, a, b, c, d, 21);
+        S_R2(d, e, a, b, c, 22);
+        S_R2(c, d, e, a, b, 23);
+        S_R2(b, c, d, e, a, 24);
+        S_R2(a, b, c, d, e, 25);
+        S_R2(e, a, b, c, d, 26);
+        S_R2(d, e, a, b, c, 27);
+        S_R2(c, d, e, a, b, 28);
+        S_R2(b, c, d, e, a, 29);
+        S_R2(a, b, c, d, e, 30);
+        S_R2(e, a, b, c, d, 31);
+        S_R2(d, e, a, b, c, 32);
+        S_R2(c, d, e, a, b, 33);
+        S_R2(b, c, d, e, a, 34);
+        S_R2(a, b, c, d, e, 35);
+        S_R2(e, a, b, c, d, 36);
+        S_R2(d, e, a, b, c, 37);
+        S_R2(c, d, e, a, b, 38);
+        S_R2(b, c, d, e, a, 39);
+        S_R3(a, b, c, d, e, 40);
+        S_R3(e, a, b, c, d, 41);
+        S_R3(d, e, a, b, c, 42);
+        S_R3(c, d, e, a, b, 43);
+        S_R3(b, c, d, e, a, 44);
+        S_R3(a, b, c, d, e, 45);
+        S_R3(e, a, b, c, d, 46);
+        S_R3(d, e, a, b, c, 47);
+        S_R3(c, d, e, a, b, 48);
+        S_R3(b, c, d, e, a, 49);
+        S_R3(a, b, c, d, e, 50);
+        S_R3(e, a, b, c, d, 51);
+        S_R3(d, e, a, b, c, 52);
+        S_R3(c, d, e, a, b, 53);
+        S_R3(b, c, d, e, a, 54);
+        S_R3(a, b, c, d, e, 55);
+        S_R3(e, a, b, c, d, 56);
+        S_R3(d, e, a, b, c, 57);
+        S_R3(c, d, e, a, b, 58);
+        S_R3(b, c, d, e, a, 59);
+        S_R4(a, b, c, d, e, 60);
+        S_R4(e, a, b, c, d, 61);
+        S_R4(d, e, a, b, c, 62);
+        S_R4(c, d, e, a, b, 63);
+        S_R4(b, c, d, e, a, 64);
+        S_R4(a, b, c, d, e, 65);
+        S_R4(e, a, b, c, d, 66);
+        S_R4(d, e, a, b, c, 67);
+        S_R4(c, d, e, a, b, 68);
+        S_R4(b, c, d, e, a, 69);
+        S_R4(a, b, c, d, e, 70);
+        S_R4(e, a, b, c, d, 71);
+        S_R4(d, e, a, b, c, 72);
+        S_R4(c, d, e, a, b, 73);
+        S_R4(b, c, d, e, a, 74);
+        S_R4(a, b, c, d, e, 75);
+        S_R4(e, a, b, c, d, 76);
+        S_R4(d, e, a, b, c, 77);
+        S_R4(c, d, e, a, b, 78);
+        S_R4(b, c, d, e, a, 79);
 
         // Add the working vars back into state
         pState[0] += a;
@@ -481,16 +576,16 @@ private:
 #endif
     }
 
-	// Member variables
-	UINT_32 m_state[5];
-	UINT_32 m_count[2];
-	UINT_32 m_reserved0[1]; // Memory alignment padding
-	UINT_8 m_buffer[64];
-	UINT_8 m_digest[20];
-	UINT_32 m_reserved1[3]; // Memory alignment padding
+    // Member variables
+    UINT_32 m_state[5];
+    UINT_32 m_count[2];
+    UINT_32 m_reserved0[1];// Memory alignment padding
+    UINT_8 m_buffer[64];
+    UINT_8 m_digest[20];
+    UINT_32 m_reserved1[3];// Memory alignment padding
 
-	UINT_8 m_workspace[64];
-	SHA1_WORKSPACE_BLOCK* m_block; // SHA1 pointer to the byte array above
+    UINT_8 m_workspace[64];
+    SHA1_WORKSPACE_BLOCK* m_block;// SHA1 pointer to the byte array above
 };
 
-#endif // SHA1_H_A545E61D43E9404E8D736869AB3CBFE7
+#endif// SHA1_H_A545E61D43E9404E8D736869AB3CBFE7
--- include/brynet/base/endian/Endian.hpp
+++ include/brynet/base/endian/Endian.hpp
@@ -1,12 +1,10 @@
 #pragma once
 
-#include <cstdint>
-#include <cassert>
-#include <cstring>
-#include <cstdbool>
-#include <cstring>
-
 #include <brynet/net/SocketLibTypes.hpp>
+#include <cassert>
+#include <cstdbool>
+#include <cstdint>
+#include <cstring>
 
 #ifdef BRYNET_PLATFORM_LINUX
 #include <endian.h>
@@ -16,125 +14,125 @@
 
 namespace brynet { namespace base { namespace endian {
 
-    inline uint64_t  hl64ton(uint64_t   hostValue)
-    {
-        uint64_t   ret = 0;
-        uint32_t   high, low;
+inline uint64_t hl64ton(uint64_t hostValue)
+{
+    uint64_t ret = 0;
+    uint32_t high, low;
 
-        low = hostValue & 0xFFFFFFFF;
-        high = (hostValue >> 32) & 0xFFFFFFFF;
-        low = htonl(low);
-        high = htonl(high);
-        ret = low;
-        ret <<= 32;
-        ret |= high;
+    low = hostValue & 0xFFFFFFFF;
+    high = (hostValue >> 32) & 0xFFFFFFFF;
+    low = htonl(low);
+    high = htonl(high);
+    ret = low;
+    ret <<= 32;
+    ret |= high;
 
-        return   ret;
-    }
+    return ret;
+}
 
-    inline uint64_t  ntohl64(uint64_t   netValue)
-    {
-        uint64_t   ret = 0;
-        uint32_t   high, low;
+inline uint64_t ntohl64(uint64_t netValue)
+{
+    uint64_t ret = 0;
+    uint32_t high, low;
 
-        low = netValue & 0xFFFFFFFF;
-        high = (netValue >> 32) & 0xFFFFFFFF;
-        low = ntohl(low);
-        high = ntohl(high);
-        ret = low;
-        ret <<= 32;
-        ret |= high;
+    low = netValue & 0xFFFFFFFF;
+    high = (netValue >> 32) & 0xFFFFFFFF;
+    low = ntohl(low);
+    high = ntohl(high);
+    ret = low;
+    ret <<= 32;
+    ret |= high;
 
-        return   ret;
-    }
+    return ret;
+}
 
 #ifdef BRYNET_PLATFORM_WINDOWS
-    inline uint64_t hostToNetwork64(uint64_t host64, bool convert = true)
-    {
-        return convert ? hl64ton(host64) : host64;
-    }
-    inline uint32_t hostToNetwork32(uint32_t host32, bool convert = true)
-    {
-        return convert ? htonl(host32) : host32;
-    }
+inline uint64_t hostToNetwork64(uint64_t host64, bool convert = true)
+{
+    return convert ? hl64ton(host64) : host64;
+}
+inline uint32_t hostToNetwork32(uint32_t host32, bool convert = true)
+{
+    return convert ? htonl(host32) : host32;
+}
 
-    inline uint16_t hostToNetwork16(uint16_t host16, bool convert = true)
-    {
-        return convert ? htons(host16) : host16;
-    }
+inline uint16_t hostToNetwork16(uint16_t host16, bool convert = true)
+{
+    return convert ? htons(host16) : host16;
+}
 
-    inline uint64_t networkToHost64(uint64_t net64, bool convert = true)
-    {
-        return convert ? ntohl64(net64) : net64;
-    }
+inline uint64_t networkToHost64(uint64_t net64, bool convert = true)
+{
+    return convert ? ntohl64(net64) : net64;
+}
 
-    inline uint32_t networkToHost32(uint32_t net32, bool convert = true)
-    {
-        return convert ? ntohl(net32) : net32;
-    }
+inline uint32_t networkToHost32(uint32_t net32, bool convert = true)
+{
+    return convert ? ntohl(net32) : net32;
+}
 
-    inline uint16_t networkToHost16(uint16_t net16, bool convert = true)
-    {
-        return convert ? ntohs(net16) : net16;
-    }
+inline uint16_t networkToHost16(uint16_t net16, bool convert = true)
+{
+    return convert ? ntohs(net16) : net16;
+}
 #elif defined BRYNET_PLATFORM_LINUX
-    inline uint64_t hostToNetwork64(uint64_t host64, bool convert = true)
-    {
-        return convert ? htobe64(host64) : host64;
-    }
-    inline uint32_t hostToNetwork32(uint32_t host32, bool convert = true)
-    {
-        return convert ? htobe32(host32) : host32;
-    }
+inline uint64_t hostToNetwork64(uint64_t host64, bool convert = true)
+{
+    return convert ? htobe64(host64) : host64;
+}
+inline uint32_t hostToNetwork32(uint32_t host32, bool convert = true)
+{
+    return convert ? htobe32(host32) : host32;
+}
 
-    inline uint16_t hostToNetwork16(uint16_t host16, bool convert = true)
-    {
-        return convert ? htobe16(host16) : host16;
-    }
+inline uint16_t hostToNetwork16(uint16_t host16, bool convert = true)
+{
+    return convert ? htobe16(host16) : host16;
+}
 
-    inline uint64_t networkToHost64(uint64_t net64, bool convert = true)
-    {
-        return convert ? be64toh(net64) : net64;
-    }
+inline uint64_t networkToHost64(uint64_t net64, bool convert = true)
+{
+    return convert ? be64toh(net64) : net64;
+}
 
-    inline uint32_t networkToHost32(uint32_t net32, bool convert = true)
-    {
-        return convert ? be32toh(net32) : net32;
-    }
+inline uint32_t networkToHost32(uint32_t net32, bool convert = true)
+{
+    return convert ? be32toh(net32) : net32;
+}
 
-    inline uint16_t networkToHost16(uint16_t net16, bool convert = true)
-    {
-        return convert ? be16toh(net16) : net16;
-    }
+inline uint16_t networkToHost16(uint16_t net16, bool convert = true)
+{
+    return convert ? be16toh(net16) : net16;
+}
 #elif defined BRYNET_PLATFORM_DARWIN
-    inline uint64_t hostToNetwork64(uint64_t host64, bool convert = true)
-    {
-        return convert ? hl64ton(host64) : host64;
-    }
-    inline uint32_t hostToNetwork32(uint32_t host32, bool convert = true)
-    {
-        return convert ? htonl(host32) : host32;
-    }
+inline uint64_t hostToNetwork64(uint64_t host64, bool convert = true)
+{
+    return convert ? hl64ton(host64) : host64;
+}
+inline uint32_t hostToNetwork32(uint32_t host32, bool convert = true)
+{
+    return convert ? htonl(host32) : host32;
+}
 
-    inline uint16_t hostToNetwork16(uint16_t host16, bool convert = true)
-    {
-        return convert ? htons(host16) : host16;
-    }
+inline uint16_t hostToNetwork16(uint16_t host16, bool convert = true)
+{
+    return convert ? htons(host16) : host16;
+}
 
-    inline uint64_t networkToHost64(uint64_t net64, bool convert = true)
-    {
-        return convert ? ntohl64(net64) : net64;
-    }
+inline uint64_t networkToHost64(uint64_t net64, bool convert = true)
+{
+    return convert ? ntohl64(net64) : net64;
+}
 
-    inline uint32_t networkToHost32(uint32_t net32, bool convert = true)
-    {
-        return convert ? ntohl(net32) : net32;
-    }
+inline uint32_t networkToHost32(uint32_t net32, bool convert = true)
+{
+    return convert ? ntohl(net32) : net32;
+}
 
-    inline uint16_t networkToHost16(uint16_t net16, bool convert = true)
-    {
-        return convert ? ntohs(net16) : net16;
-    }
+inline uint16_t networkToHost16(uint16_t net16, bool convert = true)
+{
+    return convert ? ntohs(net16) : net16;
+}
 #endif
 
-} } }
+}}}// namespace brynet::base::endian
