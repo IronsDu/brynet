15ac1a4de9caae0e6c280711dc95bf629280a8f3
IronsDu <irons.du@gmail.com>
2022-08-09 09:43:24 +0800

    update ubuntu.yml (#127)
    * workflows: add build-and-test.yml
    
    * remove scripts/travis-compiler.sh;
    remove sonar-project.properties;

--- scripts/travis-compiler.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-os_name=`uname -o`
-if [ "$os_name" = GNU/Linux ]
-then
-	build-wrapper-linux-x86-64 --out-dir bw-output make
-else
-	build-wrapper-macosx-x86 --out-dir bw-output make
-fi
